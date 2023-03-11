38793a6d4bcab6310aeb0ad2ccb844816f741ae3
Michael Schertenleib <michael@michaelschertenleib.com>
2023-03-11 22:45:32 +0100

    Added feather

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -5,11 +5,13 @@ const EPSILON = 0.0001
 
 export var speed: float
 export var radius: float
+export var rotation_speed: float
 
 var current_angle: float = 0.0
 var target_angle: float = 0.0
 var last_mouse_angle: float = 0.0
 var last_controller_angle: float = 0.0
+var target_rotation: float = 0.0
 
 
 func _normalize_angle(angle: float) -> float:
@@ -39,24 +41,40 @@ func _update_target_angle() -> void:
 	var mouse_angle = _get_mouse_angle()
 
 	if abs(last_mouse_angle - mouse_angle) > EPSILON:
-		target_angle = mouse_angle
+		self.target_angle = mouse_angle
 
 	if abs(last_controller_angle - controller_angle) > EPSILON:
-		target_angle = controller_angle
+		self.target_angle = controller_angle
 
-	last_controller_angle = controller_angle
-	last_mouse_angle = mouse_angle
+	self.last_controller_angle = controller_angle
+	self.last_mouse_angle = mouse_angle
 
 
-func _physics_process(delta: float) -> void:
-	_update_target_angle()
-	var angle_delta = _normalize_angle(target_angle - current_angle)
+func _get_angle_delta(from: float, to: float, speeed: float, delta: float) -> float:
+	from = _normalize_angle(from)
+	var angle_delta = _normalize_angle(to - from)
 
 	if angle_delta > PI:
 		angle_delta -= TAU
 
 	var max_delta = speed * delta
-	angle_delta = clamp(angle_delta, -max_delta, max_delta)
 
-	current_angle += angle_delta
-	position = Vector2(radius, 0.0).rotated(current_angle)
+	return clamp(angle_delta, -max_delta, max_delta)
+
+
+func _physics_process(delta: float) -> void:
+	_update_target_angle()
+
+	var angle_delta = _get_angle_delta(self.current_angle, self.target_angle, self.speed, delta)
+	self.current_angle += angle_delta
+	position = Vector2(radius, 0.0).rotated(self.current_angle)
+
+	if angle_delta > 0:
+		self.target_rotation = self.current_angle + PI / 2
+	elif angle_delta < 0:
+		self.target_rotation = self.current_angle - PI / 2
+
+	var rotation_delta = _get_angle_delta(
+		self.rotation, self.target_rotation, self.rotation_speed, delta
+	)
+	self.rotation += rotation_delta
