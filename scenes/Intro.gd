9d8c25dc2fdeb11ec0c266f8d34c37a2bf921c49
Michael Schertenleib <michael@michaelschertenleib.com>
2023-03-19 19:16:00 +0100

    Made start button work

--- scenes/Intro.gd
+++ scenes/Intro.gd
@@ -1,17 +1,15 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("Intro")
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _input(event):
+	if not event.is_action_pressed("ui_accept"):
+		return
+	
+	if $AnimationPlayer.is_playing() and $AnimationPlayer.current_animation_position < 7.5:
+		$AnimationPlayer.advance(10.0)
+	else:
+		get_tree().change_scene("res://scenes/Level.tscn")
